309a169e659cb6f0a147be6784f231a5a6b804fa
Idzumikatsu <pwpvpdru@yandex.ru>
2022-02-12 17:34:57 +0300

    HomeWork 5 is done ;)

--- src/main/java/HomeWorks/LessonFive/MainApp.java
+++ src/main/java/HomeWorks/LessonFive/MainApp.java
@@ -32,27 +32,40 @@ public class MainApp {
         float[] mergedArr = new float[size];
 
         Thread thread1 = new Thread(() -> {
-            synchronized (mon) {
                 System.arraycopy(arr, 0, arrLeft, 0, size / 2);
                 for (int i = 0; i < arrLeft.length; i++) {
                     arrLeft[i] = (float) (arrLeft[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
                 System.arraycopy(arrLeft, 0, mergedArr, 0, size / 2);
-            }
+                System.out.println("First thread time: " + (System.currentTimeMillis() - startTime) + " ms.");
         });
 
         Thread thread2 = new Thread(() -> {
-            synchronized (mon) {
+
                 System.arraycopy(arr, size / 2, arrRight, 0, size / 2);
                 for (int i = 0; i < arrRight.length; i++) {
                     arrRight[i] = (float) (arrRight[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
                 System.arraycopy(arrRight, 0, mergedArr, size / 2, size / 2);
-            }
+                System.out.println("Second thread time: " + (System.currentTimeMillis() - startTime) + " ms.");
+
         });
 
         thread1.start();
         thread2.start();
+
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         System.out.println("Two thread time: " + (System.currentTimeMillis() - startTime) + " ms.");
     }
